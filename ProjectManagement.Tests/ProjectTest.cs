5dfcef1d1c6c77885f2339ff1c2a1b28730a67d7
Gabriel Vitoria <gabriel.vitoria.ti@gmail.com>
2024-08-25 22:14:34 -0300

    FEAT: Test para project remove

--- ProjectManagement.Tests/ProjectTest.cs
+++ ProjectManagement.Tests/ProjectTest.cs
@@ -5,6 +5,30 @@ namespace ProjectManagement.Tests
     public class ProjectTest
     {
         [Fact]
+        public void RemoveProject()
+        {
+            try
+            {
+                var project = new Project("Projeto X", Guid.NewGuid());
+                var task = new ProjectTask(project.Id, $"Tarefa ABC ", $"Executar Hoje ", DateTime.Now, Guid.NewGuid(), Domain.TaskPriorityEnum.High);
+                task.SetStatus(Domain.TaskStatusEnum.Pending, Guid.NewGuid());
+                project.AddTask(task);
+                project.Remove();
+            }
+            catch (ApplicationException aex)
+            {
+                Assert.Equal(aex.Message, "Erro: The Project contain task Pending. Finalize pending tasks before removing a project");
+            }           
+        }
+
+        [Fact]
+        public void CreateProjectNotNull()
+        {
+            var project = new Project();    
+            Assert.NotNull(project);
+        }
+
+        [Fact]
         public void CreateProject()
         {
             var project = new Project("Projeto X", Guid.NewGuid());
